4a2e7d8e79d36639f6633ab7a43056765082f1f7
jcivelli <jcampan@gmail.com>
2024-10-01 21:01:35 -0700

    Fix typo.

--- src/Main.kt
+++ src/Main.kt
@@ -35,7 +35,7 @@ fun generateMac(key : Key, message : String) : ByteArray {
 fun printUsage() {
     println("Usage: crypto_utils <cmd> <params>\n" +
             "Commands:\n" +
-            "hash <text>\n\tHash the passed in text with SHA-256\n" +
+            "hash <text>\n\tHashes the passed in text with SHA-256\n" +
             "genSymKey\n\tCreates and returns a DES symmetric key, hex encoded.\n" +
             "genHMac <hex encoded key> <message>\n\tReturns a MAC for the given message.\n" +
             "") // TODO: finish with more commands.
